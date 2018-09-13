6ad18209e7400a22d49f2aee1557152579a1acc5
alexi001 <harshshrm246@gmail.com>
2018-09-13 10:30:59 +0530

    assignment 10

---
+++ readOperations.java
@@ -0,0 +1,33 @@
+import java.io.*;
+class readOperations{
+      public static void main(String args[]){
+                 File f=new File("/home/naman/Desktop/abc");//Since the program was done in linux, therefore .txt is not required
+                 File g=new File("/home/naman/Desktop/xyz");//Assuming file abc already had some text written
+                 char c[]=new char[100];
+                      int i,k=0;
+                 try{
+                     FileInputStream fin=new FileInputStream(f);
+
+                     i=fin.read();
+                     while(i!=-1){
+                      c[k]=(char)i;
+                     k++;
+                     i=fin.read();
+                   }
+
+                 }
+                 catch(IOException e){
+                   System.out.println(e);
+                 }
+
+                 try{
+                   FileOutputStream fout=new FileOutputStream(g);
+                   for(i=0;i<k;i++)
+                    fout.write(c[i]);
+                            System.out.println("File copied");
+                 catch(IOException e){
+                   System.out.println(e);
+
+                 }
+      }
+}
