8a902ff7656e396d951468392968e31e0a949e8c
alexi001 <harshshrm246@gmail.com>
2018-09-13 10:31:53 +0530

    Update readOperations.java

--- readOperations.java
+++ readOperations.java
@@ -1,8 +1,8 @@
 import java.io.*;
 class readOperations{
       public static void main(String args[]){
-                 File f=new File("/home/naman/Desktop/abc");//Since the program was done in linux, therefore .txt is not required
-                 File g=new File("/home/naman/Desktop/xyz");//Assuming file abc already had some text written
+                 File f=new File("/home/alexi/Desktop/abc");//Since the program was done in linux, therefore .txt is not required
+                 File g=new File("/home/alexin/Desktop/xyz");//Assuming file abc already had some text written
                  char c[]=new char[100];
                       int i,k=0;
                  try{
